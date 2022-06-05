ea3556fbf57d6bc356dd1262a453dec830e72dcf
hakan <hakan@DESKTOP-R3S6OOO>
2022-06-05 21:38:39 +0300

    ver1.6 added firebaserealtime added splash fixed manifest added which department should I go developing sensor pulse measure

--- app/src/main/java/com/example/firebasekayit/Main.java
+++ app/src/main/java/com/example/firebasekayit/Main.java
@@ -1,20 +1,13 @@
 package com.example.firebasekayit;
 
-import androidx.annotation.NonNull;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
-import android.app.ProgressDialog;
-import android.content.DialogInterface;
 import android.os.Bundle;
 
 import com.firebase.ui.database.FirebaseRecyclerOptions;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
